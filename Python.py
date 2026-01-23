b3654cca67a280ccf3dcc42dca00a7de5dac35da
dimonfres31-png <dimonfres31@gmail.com>
2026-01-23 08:55:34 +0200

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,15 @@
+
+print("Анкета про себе")
+
+name = input("Введіть ваше ім'я: ")
+age = input("Введіть ваш вік: ")
+city = input("Введіть місто проживання: ")
+school = input("Введіть назву школи: ")
+hobby = input("Введіть ваше хобі: ")
+
+print("\n--- Ваша анкета ---")
+print("Ім'я:", name)
+print("Вік:", age)
+print("Місто:", city)
+print("Школа:", school)
+print("Хобі:", hobby)
